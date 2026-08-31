610d9b6600c908228a0347bf816db7ee12ed9734
Muzzammil Ayyubi <moz-code@nomadlabs.in>
2026-08-31 06:26:06 +0530

    Mark dev builds as 0.2.0-dev so they are distinguishable from the v0.1.0 tag

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,7 @@
 package version
 
-const Version = "0.1.0"
+// Version is the build's version string.
+//
+// Development builds carry a -dev suffix so it is obvious whether a binary came
+// from a tagged release on main or from ongoing work on dev.
+const Version = "0.2.0-dev"
